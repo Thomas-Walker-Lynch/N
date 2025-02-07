98caa9a86268d26a5fbcd301056614af27141e50
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2025-02-07 09:37:41 +0000

    setup

---
+++ developer/cc🖉/environment.h
@@ -0,0 +1,9 @@
+#ifndef Mpblock·ENVIRONMENT_H
+#define Mpblock·ENVIRONMENT_H
+  #include <stdint.h>
+  #include <stdbool.h>
+
+  typedef unsigned int uint;
+  #define FREE(pt) free(pt); (pt) = NULL;
+
+#endif
